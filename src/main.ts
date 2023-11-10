8082f95018e9e2df962a606f595326beae4b6473
overthestream <njhnjh02@naver.com>
2023-11-11 06:29:44 +0900

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://team6-frontend.vercel.app',
+    origin: ['https://team6-frontend.vercel.app', 'http://localhost:3000'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
