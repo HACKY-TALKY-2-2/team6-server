a6b014762becfa4b5e0f8bd12d84b267cc654225
overthestream <njhnjh02@naver.com>
2023-11-10 21:03:00 +0900

    feat: channeltalk notification

--- src/notification/notification.controller.ts
+++ src/notification/notification.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Query, Req } from '@nestjs/common';
+import { NotificationService } from './notification.service';
+import { Request } from 'express';
 
 @Controller('notification')
-export class NotificationController {}
+export class NotificationController {
+  constructor(private notificationService: NotificationService) {}
+
+  @Post('/plain-text')
+  async sendPlainText(@Query('message') message: string) {
+    try {
+      await this.notificationService.sendPlainText(message);
+    } catch (err) {
+      console.error(err);
+      return err;
+    }
+  }
+
+  @Post('/webhook/listen')
+  async listenWebhook(@Req() req: Request) {
+    console.log(req);
+  }
+}
