f4486321bc856f8e29c9d23d8eed2438dd930fd6
overthestream <njhnjh02@naver.com>
2023-11-10 20:03:41 +0900

    feat(notification): new domain 'notification'

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-
+import { NotificationModule } from './notification/notification.module';
 @Module({
-  imports: [ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+    NotificationModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
